6c0a03d424fdf0c219f6477a632ea1265968f9f5
Pierre Mdawar <p.mdawar@gmail.com>
2020-04-22 22:03:50 +0300

    [tests] added get_workers_stats tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,12 +4,12 @@ Tests for the rq_exporter.utils module.
 """
 
 import unittest
-from unittest.mock import patch, mock_open
+from unittest.mock import patch, mock_open, Mock
 
-from redis import Redis
+from redis.exceptions import RedisError
 
 from rq_exporter import config
-from rq_exporter.utils import get_redis_connection
+from rq_exporter.utils import get_redis_connection, get_workers_stats
 
 
 class GetRedisConnectionTestCase(unittest.TestCase):
@@ -146,3 +146,86 @@ class GetRedisConnectionTestCase(unittest.TestCase):
             open.assert_called_with('/path/to/redis_pass', 'r')
 
             Redis.assert_not_called()
+
+
+class GetWorkersStatsTestCase(unittest.TestCase):
+    """Tests for the `get_workers_stats` function."""
+
+    @patch('rq_exporter.utils.Worker')
+    def test_on_redis_errors_raises_RedisError(self, Worker):
+        """On Redis connection errors, exceptions subclasses of `RedisError` will be raised."""
+        Worker.all.side_effect = RedisError('Connection error')
+
+        connection = Mock()
+
+        with self.assertRaises(RedisError):
+
+            get_workers_stats(connection)
+
+        Worker.all.assert_called_with(connection=connection)
+
+    @patch('rq_exporter.utils.Worker')
+    def test_returns_empty_list_without_workers(self, Worker):
+        """Without any available workers an empty list must be returned."""
+        Worker.all.return_value = []
+
+        connection = Mock()
+
+        workers = get_workers_stats(connection)
+
+        Worker.all.assert_called_with(connection=connection)
+
+        self.assertEqual(workers, [])
+
+    @patch('rq_exporter.utils.Worker')
+    def test_returns_worker_stats(self, Worker):
+        """When there are workers, a list of worker info dicts must be returned."""
+        q_default = Mock()
+        q_default.configure_mock(name='default')
+
+        q_high = Mock()
+        q_high.configure_mock(name='high')
+
+        q_low = Mock()
+        q_low.configure_mock(name='low')
+
+        worker_one = Mock()
+        worker_one_attrs = {
+            'name': 'worker_one',
+            'queues': [q_default],
+            'get_state.return_value': 'idle'
+        }
+        worker_one.configure_mock(**worker_one_attrs)
+
+        worker_two = Mock()
+        worker_two_attrs = {
+            'name': 'worker_two',
+            'queues': [q_high, q_default, q_low],
+            'get_state.return_value': 'busy'
+        }
+        worker_two.configure_mock(**worker_two_attrs)
+
+
+        Worker.all.return_value = [worker_one, worker_two]
+
+        connection = Mock()
+
+        workers = get_workers_stats(connection)
+
+        Worker.all.assert_called_with(connection=connection)
+
+        self.assertEqual(
+            workers,
+            [
+                {
+                    'name': 'worker_one',
+                    'queues': ['default'],
+                    'state': 'idle'
+                },
+                {
+                    'name': 'worker_two',
+                    'queues': ['high', 'default', 'low'],
+                    'state': 'busy'
+                }
+            ]
+        )
