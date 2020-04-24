c4935fbed283458dbef08d23562c889b70097695
Pierre Mdawar <p.mdawar@gmail.com>
2020-04-24 13:17:36 +0300

    Prevent running the server if the module is imported

--- rq_exporter/__main__.py
+++ rq_exporter/__main__.py
@@ -23,46 +23,52 @@ from . import register_collector
 from . import config
 
 
-logger = logging.getLogger(__package__)
-
-logging.basicConfig(
-    format = '[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s',
-    datefmt = '%Y-%m-%d %H:%M:%S',
-    level = config.LOG_LEVEL
-)
+logger = logging.getLogger(__name__)
 
 
-def signal_handler(sig, frame):
-    logger.info('Stopping the server...')
-    sys.exit(0)
+def main():
+    logging.basicConfig(
+        format = '[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s',
+        datefmt = '%Y-%m-%d %H:%M:%S',
+        level = config.LOG_LEVEL
+    )
 
-signal.signal(signal.SIGINT, signal_handler)
+
+    def signal_handler(sig, frame):
+        logger.info('Stopping the server...')
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
 
 
-PORT = 8000
+    PORT = 8000
 
-if len(sys.argv) > 1:
-    arg = sys.argv[1]
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
 
+        try:
+            PORT = int(sys.argv[1])
+        except ValueError as exc:
+            logger.error(f'Invalid port number: {arg}')
+            sys.exit(1)
+
+
+    # Register the RQ collector
     try:
-        PORT = int(sys.argv[1])
-    except ValueError as exc:
-        logger.error(f'Invalid port number: {arg}')
+        register_collector()
+    except (IOError, RedisError) as exc:
+        logger.exception('There was an error starting the RQ exporter')
         sys.exit(1)
 
+    # Start the WSGI server
+    start_wsgi_server(PORT)
 
-# Register the RQ collector
-try:
-    register_collector()
-except (IOError, RedisError) as exc:
-    logger.exception('There was an error starting the RQ exporter')
-    sys.exit(1)
+    logger.info(f'Server running on port {PORT}')
 
-# Start the WSGI server
-start_wsgi_server(PORT)
-
-logger.info(f'Server running on port {PORT}')
+    while True:
+        time.sleep(1)
 
 
-while True:
-    time.sleep(1)
+if __name__ == '__main__':
+    main()
