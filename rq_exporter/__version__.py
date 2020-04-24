8ab91d5319bdc136c0eda28a9b50770398d00967
Pierre Mdawar <p.mdawar@gmail.com>
2020-04-24 13:07:59 +0300

    Added setup.py with package metadata

---
+++ rq_exporter/__version__.py
@@ -0,0 +1,6 @@
+__version__ = '1.0.0'
+__url__ = 'https://github.com/mdawar/rq-exporter'
+__description__ = 'Prometheus exporter for Python RQ (Redis Queue)'
+__author__ = 'Pierre Mdawar'
+__author_email__ = 'pierre@mdawar.dev'
+__license__ = 'MIT'
