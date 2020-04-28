62e4673fc5d385a650d5557cc158ddeecbceabc4
Pierre Mdawar <pierre@mdawar.dev>
2020-04-28 17:08:48 +0300

    Add new config options

--- rq_exporter/config.py
+++ rq_exporter/config.py
@@ -5,17 +5,33 @@ RQ exporter configuration.
 
 import os
 
+# Defaults
+DEFAULT_HOST = '0.0.0.0'
+DEFAULT_PORT = '9726'
+DEFAULT_REDIS_URL = None
+DEFAULT_REDIS_HOST = 'localhost'
+DEFAULT_REDIS_PORT = '6379'
+DEFAULT_REDIS_DB = '0'
+DEFAULT_REDIS_PASS = None
+DEFAULT_REDIS_PASS_FILE = None
+DEFAULT_LOG_LEVEL = 'INFO'
+DEFAULT_LOG_FORMAT = '[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s'
+DEFAULT_LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
 
-REDIS_URL = os.environ.get('RQ_REDIS_URL')
+# Exporter config
+HOST = os.environ.get('RQ_EXPORTER_HOST', DEFAULT_HOST)
+PORT = os.environ.get('RQ_EXPORTER_PORT', DEFAULT_PORT)
 
+# Redis config
+REDIS_URL = os.environ.get('RQ_REDIS_URL', DEFAULT_REDIS_URL)
 # These configuration options will be ignored if the URL is set
-REDIS_HOST = os.environ.get('RQ_REDIS_HOST', 'localhost')
-REDIS_PORT = os.environ.get('RQ_REDIS_PORT', '6379')
-REDIS_DB = os.environ.get('RQ_REDIS_DB', '0')
-REDIS_PASS = os.environ.get('RQ_REDIS_PASS')
-REDIS_PASS_FILE = os.environ.get('RQ_REDIS_PASS_FILE')
+REDIS_HOST = os.environ.get('RQ_REDIS_HOST', DEFAULT_REDIS_HOST)
+REDIS_PORT = os.environ.get('RQ_REDIS_PORT', DEFAULT_REDIS_PORT)
+REDIS_DB = os.environ.get('RQ_REDIS_DB', DEFAULT_REDIS_DB)
+REDIS_PASS = os.environ.get('RQ_REDIS_PASS', DEFAULT_REDIS_PASS)
+REDIS_PASS_FILE = os.environ.get('RQ_REDIS_PASS_FILE', DEFAULT_REDIS_PASS_FILE)
 
 # Logging config
-LOG_LEVEL = os.environ.get('RQ_EXPORTER_LOG_LEVEL', 'INFO').upper()
-LOG_FORMAT = os.environ.get('RQ_EXPORTER_LOG_FORMAT', '[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s')
-LOG_DATEFMT = os.environ.get('RQ_EXPORTER_LOG_DATEFMT', '%Y-%m-%d %H:%M:%S')
+LOG_LEVEL = os.environ.get('RQ_EXPORTER_LOG_LEVEL', DEFAULT_LOG_LEVEL).upper()
+LOG_FORMAT = os.environ.get('RQ_EXPORTER_LOG_FORMAT', DEFAULT_LOG_FORMAT)
+LOG_DATEFMT = os.environ.get('RQ_EXPORTER_LOG_DATEFMT', DEFAULT_LOG_DATEFMT)
