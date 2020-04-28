e3b398904465b172bf9b1ca7c991dd308dadb376
Pierre Mdawar <pierre@mdawar.dev>
2020-04-28 11:36:56 +0300

    Remove Gunicorn logger config

--- rq_exporter/__init__.py
+++ rq_exporter/__init__.py
@@ -3,18 +3,4 @@ Python RQ Prometheus Exporter.
 
 """
 
-import logging
-
-
-logger = logging.getLogger(__name__)
-
-
-gunicorn_logger = logging.getLogger('gunicorn.error')
-
-if gunicorn_logger.hasHandlers():
-    # Set the same Gunicorn handlers and logging level
-    logger.handlers = gunicorn_logger.handlers
-    logger.setLevel(gunicorn_logger.level)
-
-
 from .exporter import register_collector, create_app
