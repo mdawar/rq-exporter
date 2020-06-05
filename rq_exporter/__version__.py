2dd5e4dd99a16f4347eb8a69bb7bb11cd7bceb11
Pierre Mdawar <pierre@mdawar.dev>
2020-06-05 09:27:23 +0300

    Version 1.2.0

--- rq_exporter/__version__.py
+++ rq_exporter/__version__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 __url__ = 'https://github.com/mdawar/rq-exporter'
 __description__ = 'Prometheus exporter for Python RQ (Redis Queue)'
 __author__ = 'Pierre Mdawar'
