13cac113c6725411d5ec1318a3497654122408d2
lmwangi <lmwangi@gmail.com>
2021-04-23 14:52:50 +0300

    move deps into setup.py

--- setup.py
+++ setup.py
@@ -7,45 +7,44 @@ cwd = os.path.abspath(os.path.dirname(__file__))
 
 # Metadata
 about = {}
-with open(os.path.join(cwd, 'rq_exporter', '__version__.py'), 'r') as f:
+with open(os.path.join(cwd, "rq_exporter", "__version__.py"), "r") as f:
     exec(f.read(), about)
 
 # Description
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
-
-# Requirements
-requirements = []
-with open(os.path.join(cwd, 'requirements.txt'), 'r') as f:
-    requirements = [l.strip() for l in f.readlines()]
 
 
 setup(
-    name = 'rq-exporter',
-    url = about['__url__'],
-    license = about['__license__'],
-    version = about['__version__'],
-    author = about['__author__'],
-    author_email = about['__author_email__'],
-    description = about['__description__'],
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    install_requires = requirements,
-    packages = ['rq_exporter'],
-    classifiers = [
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'Topic :: System :: Monitoring'
+    name="rq-exporter",
+    url=about["__url__"],
+    license=about["__license__"],
+    version=about["__version__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    description=about["__description__"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "rq>=1.8.0",
+        "redis>=3.5.3",
+        "prometheus_client>=0.8.0",
     ],
-    python_requires='>=3.6',
-    entry_points = {
-        'console_scripts': ['rq-exporter = rq_exporter.__main__:main']
-    }
+    packages=["rq_exporter"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Topic :: System :: Monitoring",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": ["rq-exporter = rq_exporter.__main__:main"]
+    },
 )
