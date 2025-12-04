93c6d83281891bdfaba9dbf4f0a7b128f9c7c037
albertjmr <148061540+albertjmr@users.noreply.github.com>
2025-12-04 16:53:22 +0000

    fix count

--- rq_exporter/utils.py
+++ rq_exporter/utils.py
@@ -112,7 +112,7 @@ def get_queue_jobs(connection, queue_name, queue_class=None):
     queue = queue_class(connection=connection, name=queue_name)
 
     return {
-        JobStatus.QUEUED: queue.get_job_count(cleanup=False),
+        JobStatus.QUEUED: queue.count,
         JobStatus.STARTED: queue.started_job_registry.get_job_count(cleanup=False),
         JobStatus.FINISHED: queue.finished_job_registry.get_job_count(cleanup=False),
         JobStatus.FAILED: queue.failed_job_registry.get_job_count(cleanup=False),
