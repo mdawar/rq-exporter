3e09be3e5b062879f9dcf4358a9ea1e8f7deb900
albertjmr <148061540+albertjmr@users.noreply.github.com>
2025-12-05 13:02:27 +0000

    fix: avoid clean up when querying queue job count (#42)

--- rq_exporter/utils.py
+++ rq_exporter/utils.py
@@ -113,11 +113,11 @@ def get_queue_jobs(connection, queue_name, queue_class=None):
 
     return {
         JobStatus.QUEUED: queue.count,
-        JobStatus.STARTED: queue.started_job_registry.count,
-        JobStatus.FINISHED: queue.finished_job_registry.count,
-        JobStatus.FAILED: queue.failed_job_registry.count,
-        JobStatus.DEFERRED: queue.deferred_job_registry.count,
-        JobStatus.SCHEDULED: queue.scheduled_job_registry.count
+        JobStatus.STARTED: queue.started_job_registry.get_job_count(cleanup=False),
+        JobStatus.FINISHED: queue.finished_job_registry.get_job_count(cleanup=False),
+        JobStatus.FAILED: queue.failed_job_registry.get_job_count(cleanup=False),
+        JobStatus.DEFERRED: queue.deferred_job_registry.get_job_count(cleanup=False),
+        JobStatus.SCHEDULED: queue.scheduled_job_registry.get_job_count(cleanup=False)
     }
 
 
